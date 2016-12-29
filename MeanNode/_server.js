f60227fcc5058647782630d456d2db07dd117dca
Rakesh <rsahu@inquesttechnologies.com>
2016-12-30 00:15:11 +0530

    added simple share js example

---
+++ MeanNode/_server.js
@@ -0,0 +1,30 @@
+﻿process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var config = require('./config/config'),
+    mongoose = require('./config/mongoose'),
+    express = require('./config/express'),
+    passport = require('./config/passport'),
+    
+    https = require('https'),
+    fs = require('fs');
+
+var db = mongoose(),
+    passport = passport(),
+    app = express();
+
+
+/*Create a https server and listen
+var options = {
+    key: fs.readFileSync('./config/keys/rakzdev-key.pem'),
+    cert: fs.readFileSync('./config/keys/rakzdev-cert.pem')
+}
+var httpsServer = https.createServer(options, app);
+httpsServer.listen(config.httpsport);
+console.log('Https' + process.env.NODE_ENV + ' server running at http://localhost:' + config.httpsport)
+*/
+
+
+//Listen to http traffic
+app.listen(config.port);
+console.log('http ' + process.env.NODE_ENV + ' server running at http://localhost:' + config.port);
+
+module.exports = app;
--- MeanNode/server.js
+++ MeanNode/server.js
@@ -1,30 +1,65 @@
-﻿process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-var config = require('./config/config'),
-    mongoose = require('./config/mongoose'),
-    express = require('./config/express'),
-    passport = require('./config/passport'),
-    
-    https = require('https'),
-    fs = require('fs');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var db = mongoose(),
-    passport = passport(),
-    app = express();
+var http = require('http');
+var Duplex = require('stream').Duplex;
+var browserChannel = require('browserchannel').server;
+var express = require('express');
+var livedb = require('livedb');
+var sharejs = require('share');
+var config = require('./config/config');
 
+var liveDBMongoClient = require('livedb-mongo');
 
-/*Create a https server and listen
-var options = {
-    key: fs.readFileSync('./config/keys/rakzdev-key.pem'),
-    cert: fs.readFileSync('./config/keys/rakzdev-cert.pem')
-}
-var httpsServer = https.createServer(options, app);
-httpsServer.listen(config.httpsport);
-console.log('Https' + process.env.NODE_ENV + ' server running at http://localhost:' + config.httpsport)
-*/
+var db = liveDBMongoClient(config.db, {
+  safe: true
+});
+var backend = livedb.client(db);
 
+// var backend = livedb.client(livedb.memory());
+var share = sharejs.server.createClient({ backend: backend });
+var app = express();
 
-//Listen to http traffic
-app.listen(config.port);
-console.log('http ' + process.env.NODE_ENV + ' server running at http://localhost:' + config.port);
+app.use(express.static(__dirname));
+app.use(express.static(sharejs.scriptsDir));
 
-module.exports = app;
+app.use(browserChannel(function (client) {
+  var stream = new Duplex({ objectMode: true });
+
+  stream._write = function (chunk, encoding, callback) {
+    if (client.state !== 'closed') {
+      client.send(chunk)
+    }
+    callback();
+  }
+
+  stream._read = function () {
+
+  };
+
+  stream.headers = client.headers;
+  stream.remoteAddress = stream.address;
+
+  client.on('message', function (data) {
+    stream.push(data);
+  });
+
+  client.on('error', function (msg) {
+    stream.stop();
+  });
+
+  client.on('close', function (reason) {
+    stream.emit('close');
+    stream.emit('end');
+    stream.end();
+  });
+
+  return share.listen(stream);
+
+}));
+
+var server = http.createServer(app);
+server.listen(7007, function (err) {
+  if (err) throw err;
+
+  console.log('Listening on http://%s:%s', server.address().address, server.address().port);
+});
